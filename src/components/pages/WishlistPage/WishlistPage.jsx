dbc8daa20d90f385154c7d89278cb7add23974f4
MonikaJhingan <67049170+MonikaJhingan@users.noreply.github.com>
2022-04-07 04:35:39 +0530

    Delete WishlistPage.jsx

--- src/components/pages/WishlistPage/WishlistPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { WishlistProduct } from 'components/WishlistProduct/WishlistProduct'
-import './WishlistPage.css'
-export const WishlistPage = () => {
-  return (
-    <div className='wishlist-page'><WishlistProduct/></div>
-  )
-}
