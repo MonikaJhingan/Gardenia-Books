0876ca7fa1bb4329b6bd34ceda79ff3031a06377
Monika Jhingan <jhinganmonika750@gmail.com>
2022-04-13 17:37:06 +0530

    error fixed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ProductProvider } from "./context/product-context";
 import { AuthProvider } from "./context/auth-context";
-import { CartProvider } from "context/cart-context";
+import { CartProvider } from "./context/cart-context";
 import { WishlistProvider } from "./context/wishlist-context";
 // Call make Server
 makeServer();
